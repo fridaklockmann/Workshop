dc7be9cf7bc3e5fad9a66bc491b21dad796f3995
Frida Klockmann <fridaklockmann@gmail.com>
2019-01-30 18:38:46 +0100

    Updates

---
+++ precache-manifest.4139fd4572d4572b4ce13ac2bedf6a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Workshop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "40d3dea54639b7aaa176",
+    "url": "/Workshop/static/js/runtime~main.40d3dea5.js"
+  },
+  {
+    "revision": "d59913fd74f677844714",
+    "url": "/Workshop/static/js/main.d59913fd.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Workshop/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d59913fd74f677844714",
+    "url": "/Workshop/static/css/main.079a5521.chunk.css"
+  },
+  {
+    "revision": "0969a767a08e5a748763ae774bb1ded4",
+    "url": "/Workshop/index.html"
+  }
+];
